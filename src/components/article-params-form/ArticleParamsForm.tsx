f612a672adf3eea708b8fbd80493aac400485f6f
Alexander Danilyuk <danilyuk25@yandex.ru>
2026-04-29 01:43:08 +0300

    Добавлены компоненты на форму

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -17,7 +17,11 @@ import styles from './ArticleParamsForm.module.scss';
 
 
 export const ArticleParamsForm = () => {
-	const [backgroundColors, setBackgroundColors] = useState<articleProps.OptionType | null>(null); /* --------   описываем общие стейты и сеттеры к стейтам ----------    --- */
+	const [backgroundColors, setBackgroundColors] = useState<articleProps.OptionType | null>(null); /* -----   описываем общие стейты и сеттеры к стейтам ------ */
+	const [contentWidthArr, setСontentWidthArr] = useState<articleProps.OptionType | null>(null);
+	const [fontFamilyOptions, setFontFamilyOptions] = useState<articleProps.OptionType | null>(null);
+	const [fontColors, setFontColors] = useState<articleProps.OptionType | null>(null);
+	
 	const [formOpen, setFormOpen] = useState(false);
 	/* const [containerStyles, setContainerStyles] = useState(styles.container);
 /* --------   Мои элементы ----------   -placeholder = string ---  .container_open */
@@ -31,13 +35,32 @@ export const ArticleParamsForm = () => {
 				[styles.container_open]: formOpen // добавляем container_open, если formOpen === true
 			})}> { /* <aside className={styles.container}> </aside> */}
 				<form className={styles.form}>
+					<h2>задайте параметры</h2>
+					<Select 
+						selected = {fontFamilyOptions}
+						options = {articleProps.fontFamilyOptions}
+						onChange = {setFontFamilyOptions}
+						title = 'шрифт'
+					/>
+					<Select 
+						selected = {fontColors}
+						options = {articleProps.fontColors}
+						onChange = {setFontColors}
+						title = 'цвет шрифта'
+					/>
+					<Separator />
 					<Select 
 						selected = {backgroundColors}
 						options = {articleProps.backgroundColors}
 						onChange = {setBackgroundColors}
-						title = 'цвет фона изменить'
+						title = 'цвет фона'
 					/>
-					<Separator />
+					<Select 
+						selected = {contentWidthArr}
+						options = {articleProps.contentWidthArr}
+						onChange = {setСontentWidthArr}
+						title = 'ширина контента'
+					/>
 					{/* --------  / Мои элементы ----------    --- */}
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
